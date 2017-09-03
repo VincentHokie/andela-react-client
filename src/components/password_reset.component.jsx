9266fde8f712ac773844a4c3721ebb3bcf424d68
Vincent Hokie <vincenthokie@gmail.com>
2017-09-03 12:11:09 +0300

    update 'password reset' component, fix syntax error

--- src/components/password_reset.component.jsx
+++ src/components/password_reset.component.jsx
@@ -66,10 +66,8 @@ handleSubmit(e) {
         //theres a form validation error(s), show it/them
         var fields = ["password", "password_confirm"];
         for( field in fields ){
-
-          if( data[fields[i]] )
+          if( data[field] )
             thiz.setState({ [field+"_error"]: data[field][0] })
-        
         }
 
       }

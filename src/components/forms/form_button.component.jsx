d90dc47f0670bf77183ee3ea25af127a732c9666
Vincent Hokie <vincenthokie@gmail.com>
2017-09-10 12:35:52 +0300

    refactor image 'alt' attributes to remove the word 'image'

--- src/components/forms/form_button.component.jsx
+++ src/components/forms/form_button.component.jsx
@@ -9,7 +9,7 @@ class FormButton extends Component {
       <div className="col-xs-12" style={{ padding: "0" }}>
       
       <button type="submit" name="Submit" className={ this.props.loading ? "btn btn-md btn-login col-xs-11" : "btn btn-md btn-login btn-block" } disabled={ this.props.loading ? "disabled" : false }>Create Shopping List</button>
-      { this.props.loading ? <img src='/static/images/loading.gif' alt="loading gif image" className="col-xs-1" /> : null }
+      { this.props.loading ? <img src='/static/images/loading.gif' alt="loading gif" className="col-xs-1" /> : null }
 
       </div>
 

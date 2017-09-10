6f4b9dcf28a52ec156d6f797edc52a9e8d931013
Vincent Hokie <vincenthokie@gmail.com>
2017-09-10 15:19:44 +0300

    add asyncrounous call to api in 'update_shopping_list.component.jsx' componentDidMount() method to get the obejct being edited

--- src/components/update_shopping_list.component.jsx
+++ src/components/update_shopping_list.component.jsx
@@ -40,6 +40,40 @@ componentDidMount(){
       GLOBAL.FLASH = false;
 
     }
+
+  var thiz = this;
+
+  thiz.setState({ loading: true  })
+
+  //get list object from database
+  fetch('https://andela-flask-api.herokuapp.com/shoppinglists/?list_id='+this.props.match.params.id,{
+      method: 'GET',
+      headers: {
+         'Authorization': 'Basic '+btoa(GLOBAL.TOKEN), 
+         'Content-Type': 'application/x-www-form-urlencoded'
+       }
+    })      // returns a promise object
+  .then((resp) => resp.json())
+  .then(function(data){
+
+    thiz.setState({ loading: false  })
+
+    //if the data is not a json object, create a general messge..otherwise, its a list object
+    if( typeof data !== "object" ){
+      thiz.setState({ general_msg: data })
+      return true;
+    }
+
+    //we got a list object back, populate state & therefore input field
+    thiz.setState({ name: data["name"]  })
+
+  
+  }) // still returns a promise object, U need to chain it again
+  .catch(function(error){
+    thiz.setState({ loading: false  })
+    thiz.setState({ general_msg: "Check your internet connection and try again" })
+  });
+
     
 }
 

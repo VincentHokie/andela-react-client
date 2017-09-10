480b368fbf7d5f830d9c6ccfe8e0928597b58860
Vincent Hokie <vincenthokie@gmail.com>
2017-09-10 12:27:33 +0300

    add 'form button' component to be used across the aaplication

---
+++ src/components/forms/form_button.component.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class FormButton extends Component {
+
+  render() {
+
+    return (
+
+      <div className="col-xs-12" style={{ padding: "0" }}>
+      
+      <button type="submit" name="Submit" className={ this.props.loading ? "btn btn-md btn-login col-xs-11" : "btn btn-md btn-login btn-block" } disabled={ this.props.loading ? "disabled" : false }>Create Shopping List</button>
+      { this.props.loading ? <img src='/static/images/loading.gif' className="col-xs-1" /> : null }
+
+      </div>
+
+    );
+  }
+
+}
+
+
+export default FormButton

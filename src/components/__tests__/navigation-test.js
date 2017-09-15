f52d26affa4541f361e2da97e4ffcfc24dfe3141
Vincent Hokie <vincenthokie@gmail.com>
2017-09-15 16:59:39 +0300

    add navigation component tests

---
+++ src/components/__tests__/navigation-test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import Navigation from '../navigation.component.jsx';
+
+import App from '../../App.js';
+
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
+
+var GLOBAL = require("../../globals.js")
+
+var nock = require("nock");
+
+describe('Navigation bar', () => {
+  let wrapper;
+
+  it('wraps content in a nav', () => {
+
+    wrapper = shallow(<Navigation />)
+    expect(wrapper.find('nav').length).toEqual(1);
+
+  });
+
+  describe('Behaviour', () => {
+
+    it('Ensure usename properly shows', () => {
+
+      wrapper = shallow(<Navigation username="SomeName" />)
+      expect(wrapper.find("ul li a").first().text()).toContain(" Welcome  SomeName");
+      
+    })
+
+
+    })
+
+  })

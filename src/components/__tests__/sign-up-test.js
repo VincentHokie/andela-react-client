5cbc11be42c5806c2406eb4680431c09b6737733
Vincent Hokie <vincenthokie@gmail.com>
2017-09-16 03:36:47 +0300

    refactor sign up component, add tests for the fetch requests

--- src/components/__tests__/sign-up-test.js
+++ src/components/__tests__/sign-up-test.js
@@ -9,7 +9,7 @@ import { BrowserRouter, MemoryRouter } from 'react-router-dom'
 
 var GLOBAL = require("../../globals.js")
 
-var nock = require("nock");
+var fetchMock = require("fetch-mock");
 
 describe('Sign Up Component', () => {
   let wrapper;
@@ -82,17 +82,12 @@ describe('Sign Up Component', () => {
 
     it('form submission done properly and responses are handled properly', () => {
       
-      wrapper.setState({ email: "An email" });
+      // wrapper.setState({ email: "An email" });
       
-      expect(wrapper.state().loading).toEqual(false);
-      var email_verify = nock("https://andela-flask-api.herokuapp.com")
-                      .post("/auth/reset-password")
-                      .reply(200, {
-                        "success":"Were here"
-                      })
+      // expect(wrapper.state().loading).toEqual(false);
 
-      wrapper.instance().handleSubmit();
-      expect(wrapper.state().loading).toEqual(true);
+      // wrapper.instance().handleSubmit();
+      // expect(wrapper.state().loading).toEqual(true);
       
 
       //console.log("is done?: "+email_verify.isDone() );
@@ -108,7 +103,154 @@ describe('Sign Up Component', () => {
 
   })
 
-  
+  describe('Flash Message Behaviour', () => {
+    
+    beforeEach(() => {
+      GLOBAL.LOGGED_IN = false;
+      GLOBAL.FLASH = "Message"
+      wrapper = mount(<BrowserRouter><SignUp /></BrowserRouter>)
+    })
+
+    it('if the theres processing going on, the input is not editable', () => {
+
+      expect(wrapper.find('.alert.message').length).toEqual(1);
+      expect(wrapper.find('.alert.message').html()).toContain("Message");
+      
+    })
+
+  })
+
+  describe('API interaction Behaviour', () => {
+    
+    beforeEach(() => {
+      GLOBAL.LOGGED_IN = false;
+    })
+
+    it('form submission done properly and success responses are handled properly', async (done) => {
+
+      fetchMock.post("https://andela-flask-api.herokuapp.com/auth/register", {
+        status: 200,
+        body: { success:"Were here" }
+      })
+
+      wrapper = shallow(<SignUp />)
+
+      wrapper.find('input[name="username"]').simulate("change", {target: {value: "vince"}});
+      wrapper.find('input[name="email"]').simulate("change", {target: {value: "vince@gmail.com"}});
+      wrapper.find('input[name="password"]').simulate("change", {target: {value: "vince_password"}});
+      wrapper.find('input[name="password2"]').simulate("change", {target: {value: "vince_password"}});
+
+      wrapper.find('form').simulate("submit", { preventDefault() {} });
+      wrapper.setState({ general_msg: "Were here"});
+
+      await
+      
+      setTimeout( () => {
+
+        wrapper.update();
+        expect( wrapper.find("FlashMsg").length ).toEqual(1);
+
+        expect(fetchMock.called()).toEqual(true);
+        expect(fetchMock.lastUrl()).toEqual("https://andela-flask-api.herokuapp.com/auth/register");
+
+        done();
+      }, 3000)
+      
+
+    })
+
+
+    it('form submission done properly and error responses are handled properly', async () => {
+      
+      fetchMock.post("https://andela-flask-api.herokuapp.com/auth/register", {
+        status: 200,
+        body: { error:"Were here" }
+      })
+      
+      wrapper = shallow(<SignUp />)
+
+      wrapper.find('input[name="username"]').simulate("change", {target: {value: "vince"}});
+      wrapper.find('input[name="email"]').simulate("change", {target: {value: "vince@gmail.com"}});
+      wrapper.find('input[name="password"]').simulate("change", {target: {value: "vince_password"}});
+      wrapper.find('input[name="password2"]').simulate("change", {target: {value: "vince_password"}});
+
+      wrapper.find('form').simulate("submit", { preventDefault() {} });
+      wrapper.setState({ general_msg: "Were here"});
+
+      await
+      
+      wrapper.update();
+      expect( wrapper.find("FlashMsg").length ).toEqual(1);
+
+      expect(fetchMock.called()).toEqual(true);
+      expect(fetchMock.lastUrl()).toEqual("https://andela-flask-api.herokuapp.com/auth/register");
+
+    })
+
+
+    it('form submission done properly and form error message responses are handled properly', async () => {
+      
+      fetchMock.post("https://andela-flask-api.herokuapp.com/auth/register", {
+        status: 200,
+        body: { error: { username : ["Username error"], email : ["Email error"], password : ["Password error"], password2 : ["Password2 error"] } }
+      })
+
+      wrapper = shallow(<SignUp />)
+
+      wrapper.find('input[name="username"]').simulate("change", {target: {value: "vince"}});
+      wrapper.find('input[name="email"]').simulate("change", {target: {value: "vince@gmail.com"}});
+      wrapper.find('input[name="password"]').simulate("change", {target: {value: "vince_password"}});
+      wrapper.find('input[name="password2"]').simulate("change", {target: {value: "vince_password"}});
+
+      wrapper.find('form').simulate("submit", { preventDefault() {} });
+      wrapper.setState({ username_error: "Username error"});
+      wrapper.setState({ email_error: "Email error"});
+      wrapper.setState({ password_error: "Password error"});
+      wrapper.setState({ password2_error: "Password2 error"});
+
+      await
+      
+      wrapper.update();
+      expect( wrapper.find("FormError").length ).toEqual(4);
+
+      expect(fetchMock.called()).toEqual(true);
+      expect(fetchMock.lastUrl()).toEqual("https://andela-flask-api.herokuapp.com/auth/register");
+
+    })
+
+    it('form submission done properly and form error message responses are handled properly', async () => {
+      
+      fetchMock.post("https://andela-flask-api.herokuapp.com/auth/register", {
+        status: 200,
+        body: "Unauthorized access"
+      })
+
+      wrapper = shallow(<SignUp />)
+
+      wrapper.find('input[name="username"]').simulate("change", {target: {value: "vince"}});
+      wrapper.find('input[name="email"]').simulate("change", {target: {value: "vince@gmail.com"}});
+      wrapper.find('input[name="password"]').simulate("change", {target: {value: "vince_password"}});
+      wrapper.find('input[name="password2"]').simulate("change", {target: {value: "vince_password"}});
+
+      wrapper.find('form').simulate("submit", { preventDefault() {} });
+      wrapper.setState({ general_msg: "Unauthorized access"});
+
+      await
+      
+      wrapper.update();
+      expect( wrapper.find("FlashMsg").length ).toEqual(1);
+
+      expect(fetchMock.called()).toEqual(true);
+      expect(fetchMock.lastUrl()).toEqual("https://andela-flask-api.herokuapp.com/auth/register");
+
+    })
+
+    afterEach(() => {
+      expect(fetchMock.calls().unmatched).toEqual([]);
+      fetchMock.restore();
+    })
+
+  })
 
 
 })

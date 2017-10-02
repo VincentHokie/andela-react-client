d6d96deec20b2ad784773a2b32639a9a5f10a995
Vincent Hokie <vincenthokie@gmail.com>
2017-10-02 23:15:35 +0300

    add back button component for easier navigation

---
+++ src/components/back_button.component.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom';
+import './css/back_button.css';
+
+class BackButton extends Component {
+
+	render() {
+
+		return (
+
+		  <div id="backButton" className="col-lg-offset-4 col-md-offset-3 col-sm-1 col-sm-offset-2 col-xs-2">
+		      <a href="/shopping-lists" className="col-xs-12 btn">
+		      	<i className="fa fa-hand-o-left"></i>Back
+		      </a>
+		  </div>
+
+		);
+	}
+}
+
+export default BackButton

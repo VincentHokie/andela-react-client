957011a1fcab2ba9569bee7d193526b80f273539
Vincent Asante <vincenthokie@gmail.com>
2017-10-05 20:26:23 +0300

    add localStorage object import and an application state instance when testing the app

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { BrowserRouter } from 'react-router-dom'
+import "./components/localStorage.js";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  localStorage.setItem("globals", JSON.stringify({"logged_in":false}));
+
   ReactDOM.render(
   	<BrowserRouter>
 		<App />

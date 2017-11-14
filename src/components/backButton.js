7f9ca5f8b65f4e93082a3e8cccc119393b7f09a5
Vincent Asante <vincenthokie@gmail.com>
2017-11-14 16:06:26 +0300

    refactor backButton component to a fully functional component

--- src/components/backButton.js
+++ src/components/backButton.js
@@ -6,7 +6,7 @@ let BackButton = props => {
 	return (
 
 		<div id="backButton" className="col-lg-offset-4 col-md-offset-3 col-sm-1 col-sm-offset-2 col-xs-2">
-			<a href="/shopping-lists" className="col-xs-12 btn" onClick={this.props.pushNavigation}>
+			<a href="/shopping-lists" className="col-xs-12 btn" onClick={props.pushNavigation}>
 				<i className="fa fa-hand-o-left"></i>Back
 		      </a>
 		</div>

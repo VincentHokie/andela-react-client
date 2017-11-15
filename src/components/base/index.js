c4a39f7f0bb323aaa0d4b2ea512d9ba8501f6a7d
Vincent Asante <vincenthokie@gmail.com>
2017-11-15 12:00:08 +0300

    move setGlobals from base component to the globals file for exportation

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import { baseUrl } from  "../../globals.js"
+import { baseUrl, setGlobals } from "../../globals.js"
 
 class Basethis extends Component {
 
   constructor() {
     super();
     this.baseUrl = baseUrl;
-    this.state = {flash: false}
+    this.state = {
+      flash: false
+    }
 
   }
 
   componentWillMount() {
 
     //set global info and window refresh/ page change
-    this.setGlobals();
+    setGlobals(this);
 
   }
 
@@ -32,50 +34,12 @@ class Basethis extends Component {
   }
 
   pushNavigation = (event) => {
-    this.props.history.push(event.target.getAttribute("href"))
+    this.props.history.push(event.target.getAttribute("href"));
   }
 
   handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   }
-
-  setGlobals = () => {
-    
-        //get global variables
-        let localData;
-
-        if( localStorage.getItem("globals") )
-          localData = JSON.parse( localStorage.getItem("globals") );
-    
-        if( localData && localData["logged_in"] )
-          this.setState({ logged_in: localData["logged_in"]  });
-    
-        if( localData && localData["token"] )
-          this.setState({ token: localData["token"]  });
-    
-        if( localData && localData["user_username"] )
-          this.setState({ user_username: localData["user_username"]  });
-    
-        if( localData && localData["flash"] )
-          this.setState({ flash: localData["flash"]  });
-
-        //once transfer of state is complete, leave no trace
-        localStorage.setItem("globals", JSON.stringify(""));
-
-
-        //add a listener to listen for page change/ refresh
-        window.addEventListener("beforeunload", function(){
-          localStorage.setItem("globals",
-            JSON.stringify({
-              logged_in: this.state.logged_in,
-              token: this.state.token,
-              user_username: this.state.user_username,
-              flash: this.state.flash
-            })
-          )
-        });
-    
-      }
 
 }
 
--- src/globals.js
+++ src/globals.js
@@ -1,1 +1,39 @@
-export const baseUrl = process.env.REACT_APP_BASE_URL
+export const baseUrl = "http://localhost:5000"; //process.env.REACT_APP_BASE_URL;
+
+export const setGlobals = (component) => {
+
+    //get global variables
+    let localData = JSON.parse(localStorage.getItem("globals"));
+
+    if (localData) {
+
+        if (localData["logged_in"])
+            component.setState({ logged_in: localData["logged_in"] });
+
+        if (localData["token"])
+            component.setState({ token: localData["token"] });
+
+        if (localData["user_username"])
+            component.setState({ user_username: localData["user_username"] });
+
+        if (localData["flash"])
+            component.setState({ flash: localData["flash"] });
+
+        //once transfer of state is complete, leave no trace
+        // localStorage.setItem("globals", JSON.stringify(""));
+
+    }
+
+    //add a listener to listen for page change/ refresh
+    window.addEventListener("beforeunload", function () {
+        localStorage.setItem("globals",
+            JSON.stringify({
+                logged_in: component.state.logged_in,
+                token: component.state.token,
+                user_username: component.state.user_username,
+                flash: component.state.flash
+            })
+        )
+    });
+
+}

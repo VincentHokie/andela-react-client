d02f4d2c8057dacab174806f38cd7dd46c3c036a
Vincent Asante <vincenthokie@gmail.com>
2017-11-15 12:11:28 +0300

    change globals to use online api

--- src/globals.js
+++ src/globals.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:5000"; //process.env.REACT_APP_BASE_URL;
+export const baseUrl = process.env.REACT_APP_BASE_URL;
 
 export const setGlobals = (component) => {
 

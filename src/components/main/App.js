e883a65481ede3c65f5bae5c92c023bc7163ee75
Vincent Asante <vincenthokie@gmail.com>
2017-11-28 14:05:55 +0300

    refactor App.js into a functional component as its not holding any state

--- src/components/main/App.js
+++ src/components/main/App.js
@@ -22,27 +22,25 @@ let vex = require('vex-js')
 vex.registerPlugin(require('vex-dialog'))
 vex.defaultOptions.className = 'vex-theme-os'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <div>
-            <Switch>
-              <Route exact path='/' component={Login} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/sign-up' component={SignUp} />
-              <Route exact path='/shopping-lists' component={ShoppingLists} />
-              <Route exact path='/shopping-list/new' component={CreateShoppingList} />
-              <Route exact path='/shopping-list/:id/edit' component={UpdateShoppingList} />
-              <Route exact path='/shopping-list/:id/item/:item_id/edit' component={UpdateShoppingListItem} />
-              <Route exact path='/email-confirmation' component={EmailConfirm} />
-              <Route exact path='/password-reset/:token' component={PasswordReset} />
-              <Route exact path='*' component={NotFound} />
-            </Switch>
-          </div>
-      </div>
-    );
-  }
-}
+export default props => {
 
-export default App;
+  return (
+    <div className="App">
+        <div>
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/sign-up' component={SignUp} />
+            <Route exact path='/shopping-lists' component={ShoppingLists} />
+            <Route exact path='/shopping-list/new' component={CreateShoppingList} />
+            <Route exact path='/shopping-list/:id/edit' component={UpdateShoppingList} />
+            <Route exact path='/shopping-list/:id/item/:item_id/edit' component={UpdateShoppingListItem} />
+            <Route exact path='/email-confirmation' component={EmailConfirm} />
+            <Route exact path='/password-reset/:token' component={PasswordReset} />
+            <Route exact path='*' component={NotFound} />
+          </Switch>
+        </div>
+    </div>
+  );
+
+}
